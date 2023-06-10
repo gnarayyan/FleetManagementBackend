0f7db09d13031f1164114c2854bb74b319479326
Narayan Gautam <ng4111894@gmail.com>
2023-06-10 17:48:17 +0545

    create schedule app

--- country_info/models.py
+++ country_info/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Province(models.Model):
+    name = models.CharField(max_length=50)
+
+class District(models.Model):
+    name = models.CharField(max_length=50)
+    province =  models.ForeignKey(Province, on_delete=models.CASCADE)
+    
+
+class Municipality(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=1, choices=[('R', 'Rural Municipality (गाउँपालिका)'), ('m', 'Municipality (नगरपालिका)'), (
+        'M', 'Metropolitan City (महानगरपालिका)'), ('S', 'Sub - Metropolitan City (उप-महानगरपालिका)')], default='m')
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+
+
+
